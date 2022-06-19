edec85bf69cff313ecee32191bbb4cf5fcc94b97
NightCatrin <ektant9@gmail.com>
2022-06-19 20:53:48 +0700

    Отслеживаем команду start. Присылаем сообщение с приветсвием. Добавляя имя и фамилию

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-# This is a sample Python script.
+# -*- coding: utf-8 -*-
+from telebot import *
+from telebot import types
 
-print('Hello world')
+
+#Параметр - токен необходимого бота с которым будем взаимодействовать
+bot = telebot.TeleBot('')
+
+#Отслеживаем команды
+@bot.message_handler(commands=['start'])
+def start(message):
+    #Добавляем имя и фамилию пользоватея в сообщение с приветсвием
+    name = f'Привет, <b>{message.from_user.first_name} {message.from_user.last_name}</b>. Этот бот создан для тебя!'
+    bot.send_message(message.chat.id, name, parse_mode='html')
+
+
+
+
+
+bot.polling(none_stop=True)
