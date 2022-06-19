1903d713e0c2dc65eff06192651351cf3effa4b8
NightCatrin <ektant9@gmail.com>
2022-06-19 23:20:47 +0700

    Работа над кнопкой Погода. Вариант выбора погоды.

--- main.py
+++ main.py
@@ -27,10 +27,9 @@ def start(message):
 #Отслеживаем сообщения пользователя. Создаем кнопки по полем ввода текста
 @bot.message_handler(content_types=['text'])
 def button(message):
-    if message.text == "Да" or message.text == "да":
+    if message.text == "Да" or message.text == "да" or message.text == "Вернуться назад":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-        btn1 = types.KeyboardButton('Погода сегодня')
-        btn2 = types.KeyboardButton('Погода сейчас')
+        btn1 = types.KeyboardButton('Погода')
         btn3 = types.KeyboardButton('Запланировать событие')
         btn4 = types.KeyboardButton('Посмотреть событие')
         btn5 = types.KeyboardButton('Открыть диалог с Катей')
@@ -38,13 +37,30 @@ def button(message):
         btn7 = types.KeyboardButton('Сколько дней осталось до зп?')
         btn8 = types.KeyboardButton('Сколько осталось спать?')
         btn9 = types.KeyboardButton('Насколько ты молодец')
-        markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9)
+        markup.add(btn1, btn3, btn4, btn5, btn6, btn7, btn8, btn9)
         bot.send_message(message.chat.id, 'Отлично! Выбирай.', reply_markup=markup)
+        bot.register_next_step_handler(message, choice_of_answer)
     elif message.text == "Нет" or message.text == "нет":
         bot.send_message(message.chat.id, 'Ответ неправильный. Давай еще раз...')
     else:
-        bot.send_message(message.chat.id, "Напиши, пожалуйста, Да или Нет")
+        bot.send_message(message.chat.id, 'Напиши Да или Нет!')
 
+def choice_of_answer(message):
+    if message.text == "Погода":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        btn1 = types.KeyboardButton('Погода сегодня')
+        btn2 = types.KeyboardButton('Погода сейчас')
+        btn3 = types.KeyboardButton('Вернуться назад')
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.chat.id, 'а теперь выбери что-то из этого', reply_markup=markup)
+        bot.register_next_step_handler(message, weather)
 
+def weather(message):
+    if message.text == "Погода сегодня":
+        bot.send_message(message.chat.id, 'Сейчас расскажу тебе погоду в Новосибирске на сегодня')
+    elif message.text == "Погода сейчас":
+        bot.send_message(message.chat.id, 'Сейчас расскажу тебе погоду в Новосибирске на данный момент')
+    elif message.text == "Вернуться назад":
+        button(message)
 
 bot.polling(none_stop=True)
