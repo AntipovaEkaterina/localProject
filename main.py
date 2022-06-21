fe6b887fcbcf23b86c2a70dcd65d88ee62d95d01
NightCatrin <ektant9@gmail.com>
2022-06-21 22:38:21 +0700

    Начинаем обрабатывать кнопки

--- main.py
+++ main.py
@@ -34,17 +34,15 @@ def answer_inline(call):
     if call.data == "yes":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
         btn1 = types.KeyboardButton('Погода')
-        btn3 = types.KeyboardButton('Запланировать событие')
-        btn4 = types.KeyboardButton('Посмотреть событие')
-        btn5 = types.KeyboardButton('Открыть диалог с Катей')
-        btn6 = types.KeyboardButton('Открыть сообщение от Кати')
-        btn7 = types.KeyboardButton('Сколько дней осталось до зп?')
-        btn8 = types.KeyboardButton('Сколько осталось спать?')
-        btn9 = types.KeyboardButton('Насколько ты молодец')
-        markup.add(btn1, btn3, btn4, btn5, btn6, btn7, btn8, btn9)
+        btn2 = types.KeyboardButton('Событие')
+        btn3 = types.KeyboardButton('Катя')
+        btn4 = types.KeyboardButton('Сколько дней осталось до зп?')
+        btn5 = types.KeyboardButton('Сколько осталось спать?')
+        btn6 = types.KeyboardButton('Насколько ты молодец')
+        markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
         bot.send_message(call.message.chat.id, 'Отлично! Выбирай.', reply_markup=markup)
     if call.data == "no":
-        bot.send_message(call.message.chat.id, 'Ответ неправильный. Давай еще раз...')
+        bot.send_message(call.message.chat.id, 'Ответ неправильный. Давай еще раз... Нажимай на /start')
 
 @bot.message_handler(content_types=['text'])
 def choice_of_answer(message):
@@ -54,8 +52,35 @@ def choice_of_answer(message):
         btn2 = types.KeyboardButton('Погода сейчас')
         # btn3 = types.KeyboardButton('Вернуться назад')
         markup.add(btn1, btn2)
-        bot.send_message(message.chat.id, 'а теперь выбери что-то из этого', reply_markup=markup)
+        bot.send_message(message.chat.id, 'Что именно тебя интересует?', reply_markup=markup)
         bot.register_next_step_handler(message, weather)
+    elif message.text == "Событие":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        btn1 = types.KeyboardButton('Запланировать событие')
+        btn2 = types.KeyboardButton('Посмотреть событие')
+        # btn3 = types.KeyboardButton('Вернуться назад')
+        markup.add(btn1, btn2)
+        bot.send_message(message.chat.id, 'Что ты хочешь сделать?', reply_markup=markup)
+        bot.register_next_step_handler(message, event)
+    elif message.text == "Катя":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        btn1 = types.KeyboardButton('Открыть диалог с Катей')
+        btn2 = types.KeyboardButton('Открыть сообщение от Кати')
+        # btn3 = types.KeyboardButton('Вернуться назад')
+        markup.add(btn1, btn2)
+        bot.send_message(message.chat.id, 'Что ты хочешь сделать?', reply_markup=markup)
+        # bot.register_next_step_handler(message, )
+    elif message.text == "Сколько дней осталось до зп?":
+        bot.send_message(message.chat.id, 'Скоро получишь! Осталось чуть-чуть')
+    elif message.text == "Сколько осталось спать?":
+        bot.send_message(message.chat.id, 'Самое популярное занятие перед сном — считать, сколько часов ты проспишь, если уснешь прямо сейчас.'
+                                          'Как Хорошо, что у тебя есть этот бот.')
+    elif message.text == "Насколько ты молодец":
+        bot.send_message(message.chat.id, 'Ты молодец на 100%')
+    else:
+        bot.send_message(message.chat.id, 'Тут тебе придется немного подождать, я еще не все сделала.\n '
+                                          'Поиграй пока с другими кнопками.<b> Для этого тебе надо нажать на /start</b>', parse_mode='html')
+
 
 def weather(message):
     if message.text == "Погода сегодня":
@@ -63,5 +88,13 @@ def weather(message):
     elif message.text == "Погода сейчас":
         bot.send_message(message.chat.id, 'Сейчас расскажу тебе погоду в Новосибирске на данный момент')
     # elif message.text == "Вернуться назад":
+
+def event(message):
+    if message.text == "Запланировать событие":
+        bot.send_message(message.chat.id, 'Напиши, что ты хочешь запланировать.')
+    elif message.text == "Посмотреть событие":
+        bot.send_message(message.chat.id, 'Что тебя интересует?')
+
+
 
 bot.polling(none_stop=True)
