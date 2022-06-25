5d885258c6947a2492c74d96874b6e3f1ef52af8
NightCatrin <ektant9@gmail.com>
2022-06-26 01:52:28 +0700

    Вынесла в отдельную функцию создание кнопок

--- main.py
+++ main.py
@@ -7,14 +7,23 @@ import day_pay
 #Параметр - токен необходимого бота с которым будем взаимодействовать
 bot = telebot.TeleBot('')
 
-#Отслеживаем команду /start
-@bot.message_handler(commands=['start'])
-def start(message):
-    #Добавляем InLine клавиатуру к сообщению
+def create_inline_button():
     markup = types.InlineKeyboardMarkup()
     button1 = types.InlineKeyboardButton(text="Да", callback_data="yes")
     button2 = types.InlineKeyboardButton(text="Нет", callback_data="no")
     markup.add(button1, button2)
+    return markup
+
+def create_button(list):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    button_list = [types.KeyboardButton(text=x) for x in list]
+    keyboard.add(*button_list)
+    return keyboard
+
+#Отслеживаем команду /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    #Добавляем InLine клавиатуру к сообщению
     #Добавляем имя и фамилию пользоватея в сообщение с приветсвием
     name = f'<b>Привет, {message.from_user.first_name} {message.from_user.last_name}.\n' \
            f'Благодаря этому боту ты сможешь узнать:</b>\n\n' \
@@ -28,50 +37,30 @@ def start(message):
            f'8.Сколько тебе осталось спать\n' \
            f'9.Насколько ты молодец\n' \
            f'\n<b>Скорее нажимай на кнопки и удивляйся моими способностями\n\nТы готов(Да/Нет)?</b>'
-    bot.send_message(message.chat.id, name, parse_mode='html', reply_markup=markup)
+    bot.send_message(message.chat.id, name, parse_mode='html', reply_markup=create_inline_button())
 
 @bot.callback_query_handler(func=lambda call: True)
 def answer_inline(call):
     if call.data == "yes":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-        btn1 = types.KeyboardButton('Погода')
-        btn2 = types.KeyboardButton('Событие')
-        btn3 = types.KeyboardButton('Катя')
-        btn4 = types.KeyboardButton('Сколько дней осталось до зп?')
-        btn5 = types.KeyboardButton('Сколько осталось спать?')
-        btn6 = types.KeyboardButton('Насколько ты молодец')
-        markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
-        bot.send_message(call.message.chat.id, 'Отлично! Выбирай.', reply_markup=markup)
+        list = ['Погода', 'Событие', 'Катя', 'Сколько дней осталось до зп?', 'Сколько осталось спать?', 'Насколько ты молодец']
+        bot.send_message(call.message.chat.id, 'Отлично! Выбирай.', reply_markup=create_button(list))
     if call.data == "no":
         bot.send_message(call.message.chat.id, 'Ответ неправильный. Давай еще раз... Нажимай на /start')
 
 @bot.message_handler(content_types=['text'])
 def choice_of_answer(message):
     if message.text == "Погода":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-        btn1 = types.KeyboardButton('Погода сегодня')
-        btn2 = types.KeyboardButton('Погода сейчас')
-        btn3 = types.KeyboardButton('Вернуться назад')
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, 'Что именно тебя интересует?', reply_markup=markup)
+        list = ['Погода сегодня', 'Погода сейчас', 'Вернуться назад' ]
+        bot.send_message(message.chat.id, 'Что именно тебя интересует?', reply_markup=create_button(list))
         bot.register_next_step_handler(message, weather)
     elif message.text == "Событие":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-        btn1 = types.KeyboardButton('Запланировать событие')
-        btn2 = types.KeyboardButton('Посмотреть событие')
-        btn3 = types.KeyboardButton('Вернуться назад')
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, 'Что ты хочешь сделать?', reply_markup=markup)
+        list = ['Запланировать событие', 'Посмотреть событие', 'Вернуться назад']
+        bot.send_message(message.chat.id, 'Что ты хочешь сделать?', reply_markup=create_button(list))
         bot.register_next_step_handler(message, event)
     elif message.text == "Катя":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-        btn1 = types.KeyboardButton('Открыть диалог с Катей')
-        btn2 = types.KeyboardButton('Открыть сообщение от Кати')
-        btn3 = types.KeyboardButton('Вернуться назад')
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, 'Что ты хочешь сделать?', reply_markup=markup)
+        list = ['Открыть диалог с Катей', 'Открыть сообщение от Кати', 'Вернуться назад']
+        bot.send_message(message.chat.id, 'Что ты хочешь сделать?', reply_markup=create_button(list))
         bot.register_next_step_handler(message, dialog_katya)
-
     elif message.text == "Сколько дней осталось до зп?":
         day = day_pay.day_salary()
         if day == 0:
